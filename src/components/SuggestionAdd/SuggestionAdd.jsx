50b6ee36ae1dcf7473cfc4f4c64f00de39157c60
Mysik <Mishamysik@gmail.com>
2022-05-16 14:29:49 +0300

    home suggestion add feedback btn

--- src/components/SuggestionAdd/SuggestionAdd.jsx
+++ src/components/SuggestionAdd/SuggestionAdd.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './suggestion-add.scss';
+import {Link} from "react-router-dom";
 
 const SuggestionAdd = () => {
     return (
-        <div>
-            SuggestionAdd
-        </div>
+         <Link to='/feedback/form' className='suggestion--add-btn' >
+             + Add Feedback
+         </Link>
     );
 };
 
