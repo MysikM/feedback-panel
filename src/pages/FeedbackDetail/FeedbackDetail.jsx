cf3d08fd7b156ea525b9e8dad68e841c62f40ede
Mysik <Mishamysik@gmail.com>
2022-05-17 11:52:21 +0300

    feedback detail suggestion item add

--- src/pages/FeedbackDetail/FeedbackDetail.jsx
+++ src/pages/FeedbackDetail/FeedbackDetail.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './feedback-detail.scss';
 import {useParams} from "react-router-dom";
 import DetailNavigation from "../../components/DetailNavigation/DetailNavigation";
+import SuggestionItem from "../../components/SuggestionItem/SuggestionItem";
+import {useSelector} from "react-redux";
 
 const FeedbackDetail = () => {
     const {id} = useParams();
+
+    const {productRequests} = useSelector(state => state.suggestion);
+    const [suggestionItem, setSuggestionItem] = useState(productRequests.find(item => item.id === +id));
+
+    useEffect(()=>{
+        setSuggestionItem(productRequests.find(item => item.id === +id));
+    },[id])
     return (
         <section className='detail'>
             <div className="detail--container">
                 <DetailNavigation />
+                <SuggestionItem {...suggestionItem} />
             </div>
         </section>
     );
