1d78a711e0b07050cd34fcaff6bec2caf4f05a4a
Mysik <Mishamysik@gmail.com>
2022-05-19 10:27:24 +0300

    change load logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Navigations from "./navigation/Navigations";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {initialSuggestion} from "./store/slices/suggestionSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const {sortProductRequest} = useSelector(state => state.suggestion);
 
   useEffect(()=>{
